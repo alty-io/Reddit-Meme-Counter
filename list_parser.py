521defdafa897531172132346b28b8f52470f057
sullivantyl <sullivan.tyl@gmail.com>
2016-06-01 21:05:15 -0400

    Added test list and parser

---
+++ list_parser.py
@@ -0,0 +1,14 @@
+'''
+This code gathers information from a csv file and utilizes the strash code
+'''
+
+import csv
+from strash import strash
+
+with open("memes.csv") as infile:
+    rd = csv.reader(infile)
+    memes = list()
+    for row in rd:
+        memes.extend(row)
+
+hashed_memes = strash(memes)
