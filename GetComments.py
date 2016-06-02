c02e7f0f074d81b1c072c054c99585514de84b9d
srayfin0x <sullivan.tyl@gmail.com>
2016-06-02 15:26:29 -0400

    added GetComments.py

---
+++ GetComments.py
@@ -0,0 +1,22 @@
+
+import praw
+from praw.helpers import comment_stream
+
+def getComments():
+
+    user_agent = "User-Agent: CommentGrabber v0.1 (by /u/sjuha and /u/teefour)"
+    r = praw.Reddit(user_agent=user_agent)
+
+    comments = list()
+
+    for comment in comment_stream(r, 'all', limit=100):
+        comments.append(comment)
+
+        if len(comments) > 100:
+            return comments
+            break
+
+
+comments = getComments()
+for comment in comments:
+    print(comment)
